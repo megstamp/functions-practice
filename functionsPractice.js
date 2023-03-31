8326f3e8b343fe0a1088f2f59063561f0a3c7c8b
Meghann Stamper <meg.stamp@yahoo.com>
2023-03-30 20:01:38 -0400

    more function practice

--- functionsPractice.js
+++ functionsPractice.js
@@ -1,66 +1,88 @@
 // 1. Write a function to add two numbers together, using ES5 syntax (functional
 // declaration, the main way we have shown to write functions so far).
+
 function addTwoNumbers(number1, number2) {
-    const add = number1 + number2
-    console.log(add)
+  const add = number1 + number2;
+  console.log(add);
 }
 
-addTwoNumbers(7,17)
+addTwoNumbers(7, 17);
 
 // 2. Create a function that takes in a number as a parameter and prints the
 // square of that number.
 function squareNumber(number1) {
-    const double = number1 * number1
-    console.log(double)
+  const double = number1 * number1;
+  console.log(double);
 }
-squareNumber(6)
+squareNumber(6);
 
 // 3. Write a function to divide two numbers.
 function divideTwoNumbers(number1, number2) {
-    const halve = number1 / number2
-   console.log(halve)
+  const halve = number1 / number2;
+  console.log(halve);
 }
-divideTwoNumbers (12,6)
+divideTwoNumbers(12, 6);
 
 // 4. Write a function that multiplies two numbers. Invoke the function.
 function multiplyTwoNumbers(number1, number2) {
-    const times = number1 * number2
-    console.log (times)
+  const times = number1 * number2;
+  console.log(times);
 }
-multiplyTwoNumbers (10, 12)
+multiplyTwoNumbers(10, 12);
 
-
-// 5. Write a function that converts weeks into hours. Return the result. 
+// 5. Write a function that converts weeks into hours. Return the result.
 // Invoke the function.
-function turnWeeksintoHours (weeks, days, hours) {
-    const result = weeks * days * hours
-    return result;
-}
+// function turnWeeksintoHours (weeks, days, hours) {
+//     const result = weeks * days * hours
+//     return result;
+// }
 
-const result = turnWeeksintoHours (10, 7, 24)
-console.log (result);
+// const result = turnWeeksintoHours (10, 7, 24)
+// console.log (result);
 
-const weeksInYear = 52
-const hoursInYear = turnWeeksintoHours(weeksInYear, 7, 24)
-
-console.log("There are " + hoursInYear + " hours in a year.")
+// console.log("There are " + hoursInYear + " hours in a year.")
 
 //
-
-
-
-
-
+// function turnAnotherWeekIntoHours (weeks) {
+//     const days = weeks * 7
+//     const hours = days * 24
+// }
 
 // 6. Write a function that takes two parameters, a total price and a tax rate,
 // and calculates the cost of tax.
+
+ function costOfTax (totalPrice, taxRate) {
+    let firstNumber = totalPrice * taxRate
+       console.log (firstNumber );
+    return (firstNumber);
+ }
+
+ costOfTax(30, .07)
 
 // 7. Write a function that takes a person’s name as a parameter and prints the
 // message “Hello ___!”, including that person’s name, to the console. (Example:
 // “Hello Amanda!“)
 
+// function printName (firstName) {
+//     console.log ("Hello " + firstName)
+// }
+// printName ("Meghann")
+
 // 8. Write a function that checks if someone is of drinking age, and outputs
 // true if they are, and false if they are not.
+
+function drinkingAge(age) {
+  if (age >= 21) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
+
+let age = 25;
+drinkingAge (age)
+
+
 
 // 9. Write a function that takes two parameters, a persons name and a color, and
 // prints the message “___ s favorite color is ____”, including that persons name
@@ -68,10 +90,10 @@ console.log("There are " + hoursInYear + " hours in a year.")
 
 // 10. Create a function that takes in two numbers and prints which number is larger.
 
-// 11. Write a function named assignGrade, that takes one argument, 
+// 11. Write a function named assignGrade, that takes one argument,
 // a number score (example: 90).
 // Return a grade for the score, either “A,” “B,” “C,” “D,” or “F.”
-// Invoke that function for a few different scores and log the result 
+// Invoke that function for a few different scores and log the result
 // to make sure it works for each case.
 
 // 12. Write a function to get the first element in an array.
@@ -89,10 +111,9 @@ console.log("There are " + hoursInYear + " hours in a year.")
 // 17. You are given a “car” object that includes three properties: year, model, and
 // color. Write a “for” loop to console log each property of the object.
 
-// 18. Create a function that removes the first element of the array below 
-// and adds “kiwi” to the end of the array. let favoriteFruits 
+// 18. Create a function that removes the first element of the array below
+// and adds “kiwi” to the end of the array. let favoriteFruits
 // = [“mango”, “lychee”, “strawberry”, “papaya”]
 
 // 19. Write a function that takes an array of numbers and returns an array of only
 // the even numbers.
-
